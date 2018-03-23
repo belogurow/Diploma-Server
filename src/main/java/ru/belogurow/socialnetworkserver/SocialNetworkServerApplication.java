4f80491f265553b125831d2f61649343fa1fcadd
Alex Belogurow <alexbelogur10@gmail.com>
2018-03-23 23:24:14 +0300

    Add project

---
+++ src/main/java/ru/belogurow/socialnetworkserver/SocialNetworkServerApplication.java
@@ -0,0 +1,12 @@
+package ru.belogurow.socialnetworkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkServerApplication.class, args);
+	}
+}
